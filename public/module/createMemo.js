d3bdab231c0fd06c7d1927a043a3b91a271c450f
승희 <bangseunghee1108@gmail.com>
2023-12-08 00:31:32 +0900

    createMemo 작성 후 export

---
+++ public/module/createMemo.js
@@ -0,0 +1,21 @@
+export function createMemo(title, content, timestamp) {
+  const selectors = {
+    memoForm: document.getElementById("memo-Form"),
+    title: document.getElementById("title"),
+    content: document.getElementById("content"),
+    saveBtn: document.getElementById("saveBtn"),
+    memoList: document.getElementById("memo-list"),
+  }
+  
+  const memo = document.createElement("div");
+  memo.classList.add("memo-item");
+
+  const memoContent = `
+    <h2>${title}</h2>
+    <p>${content}</p>
+    <p>${timestamp}</p>
+  `;
+
+  memo.innerHTML = memoContent;
+  selectors.memoList.appendChild(memo);
+}
